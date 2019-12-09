eea8034a1bfb18f96964ef0861fea7a3e610bdf3
Tikondra <tikondra47@gmail.com>
2019-12-09 22:18:04 +0400

    save

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 $(function(){
- 
+
  $( "#slider-range" ).slider({
  range: true,
  min: 15000,
  max: 100000,
+ step: 1000,
  values: [ 30000, 60000 ],
  slide: function( event, ui ) {
  $( "#price" ).val( "от ₽ " + ui.values[ 0 ] + " - до ₽ " + ui.values[ 1 ] );
@@ -11,23 +12,23 @@ $(function(){
  });
  $( "#price" ).val( "от ₽ " + $( "#slider-range" ).slider( "values", 0 ) +
  " - до ₽ " + $( "#slider-range" ).slider( "values", 1 ) );
- 
+
 });
 
 $(function(){
- 
+
  $( "#slider-range2" ).slider({
  range: true,
- min: 15000,
- max: 100000,
- values: [ 30000, 60000 ],
+ min: 5,
+ max: 300,
+ values: [ 50, 200 ],
  slide: function( event, ui ) {
- $( "#area" ).val( "от ₽ " + ui.values[ 0 ] + " - до ₽ " + ui.values[ 1 ] );
+ $( "#area" ).val( "от кв.м " + ui.values[ 0 ] + " - до кв.м " + ui.values[ 1 ] );
  }
  });
- $( "#area" ).val( "от ₽ " + $( "#slider-range2" ).slider( "values", 0 ) +
- " - до ₽ " + $( "#slider-range2" ).slider( "values", 1 ) );
- 
+ $( "#area" ).val( "от кв.м " + $( "#slider-range2" ).slider( "values", 0 ) +
+ " - до кв.м " + $( "#slider-range2" ).slider( "values", 1 ) );
+
 });
 
 function getFileName () {
